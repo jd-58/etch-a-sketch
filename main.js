e05979f728ff3aafaf99bec08eec38c31fdc19af
jd-58 <138058948+jd-58@users.noreply.github.com>
2023-08-29 16:06:17 -0400

    Added color change functionality
    Hard coded grid to be 4x4, and color to change to red
    on mouse hover.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gridContainer = document.getElementById("gridContainer");
-let gridArray = [];
+
 
 //let gridAmount = 256;  starting value of 16x16
 /*for (var i = 1; i<= gridAmount; i++) {
@@ -14,13 +14,30 @@ let gridArray = [];
 for (let i=1; i<= gridAmount; i++) {
     let gridDiv = document.createElement("div");
     gridDiv.setAttribute("id", "gridNum" + id);
-    gridDiv.setAttribute("class", "grid")
-    gridDiv.style.color = "black"
+    gridDiv.setAttribute("class", "grid");
+    gridDiv.style.color = "black";
+    gridDiv.addEventListener('mouseover', changeGridColor)
     gridContainer.appendChild(gridDiv);
     id++;
+
 }
 }
+
+
 
 createGrid(16);
+function changeGridColor(e) {
+Object.assign(e.target.style, {
+    backgroundColor : "red" });
+}
+/*let gridArray = document.getElementsByClassName("grid");
+for (let i=1; i<= gridArray.length; i++) {
+    currentGridNumber = "gridNum" + i;
+    String(currentGridNumber);
+    let gridPixel = document.getElementById(currentGridNumber);
+    console.log(gridPixel)
+    // gridPixel.addEventListener("mouseenter", changeColor);
+}
+// gridNum1.addEventListener("mouseenter", gridNum1.style.backgroundColor = "red")
 
-
+*/
